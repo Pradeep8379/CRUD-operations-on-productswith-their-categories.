dcc5352e75532c246d29d2688469ca339ee82336
pradeep <pradeeppatil8379@gmail.com>
2023-04-13 18:59:51 +0530

    cyclic deployment

--- src/index.js
+++ src/index.js
@@ -17,6 +17,24 @@ mongoose.connect('mongodb+srv://PradeepPatil:vp0T2toXsM1QqQAo@cluster0.h3sgz2m.m
 
 app.use('/', route)
 
+//vercel
+
+if (process.env.NODE_ENV == "production") {
+    const path = require("path");
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  
+    app.get("*", function (_, res) {
+      res.sendFile(
+        path.join(__dirname, "../client/build/index.html"),
+        function (err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+        }
+      );
+    });
+  }
+
 
 app.listen(process.env.PORT || 4000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 4000))
